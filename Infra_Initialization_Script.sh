a78c6700111ee2e3af20caadec0fa50ea197dca9
thakurnishu <thakurnishant5102003@gmail.com>
2023-07-15 19:12:10 +0530

    Modified it to Create full Infra

--- Infra_Initialization_Script.sh
+++ Infra_Initialization_Script.sh
@@ -2,23 +2,46 @@
 
 RG="10_Weeks_Of_CloudOps"
 Location="centralindia"
+AccountName="myfirstweekofcloudops"
+ProfileName="CDN-First-Week"
 
 echo "-------------------------------------"
 echo "|      Creating Resource Group      |"
 echo "-------------------------------------"
-
 az group create \
   --name $RG \
   --location $Location
 
+
 echo "-------------------------------------"
 echo "|  Creating Azure Storage Account   |"
 echo "-------------------------------------"
-
 az storage account create \
-  --name "myfirstweekofcloudops" \
+  --name $AccountName \
   --resource-group $RG \
   --location $Location \
   --sku Standard_LRS \
   --kind StorageV2
 
+
+echo "-------------------------------------"
+echo "|      Enabling Static-Website      |"
+echo "-------------------------------------"
+az storage blob service-properties update \
+  --account-name $AccountName \
+  --static-website \
+  --index-document "index.html" \
+  --404-document "error.html"
+
+
+echo "-------------------------------------"
+echo "|        Creating CDN Profile       |"
+echo "-------------------------------------"
+az cdn profile create \
+  --name $ProfileName \
+  --resource-group $RG \
+  --sku Standard_Microsoft
+
+echo "--------------------------------------------------------------------------------"
+echo "| Upload Website Content and Create CDN Endpoint with above create CDN Profile |"
+echo "-------------------------------------------------------------------------------"
