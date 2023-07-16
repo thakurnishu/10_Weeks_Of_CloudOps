c225f788b616a547905e49eaecca67bb5ea5ae45
thakurnishu <thakurnishant5102003@gmail.com>
2023-07-16 17:16:11 +0530

    Modified pipeline script

--- pipeline_script.sh
+++ pipeline_script.sh
@@ -6,17 +6,7 @@ RG="10_Weeks_Of_CloudOps"
 Connection_String=$(az storage account show-connection-string --name $AccountName --resource-group $RG --query connectionString -o tsv)
 az storage blob upload-batch \
   -d "\$web" \
-  -s "../10_Weeks_Of_CloudOps" \
+  -s "website" \
   --connection-string $Connection_String \
   --overwrite \
-  --pattern "*.html"
-
-sleep 10
-
-az storage blob upload-batch \
-  -d "\$web" \
-  -s "../10_Weeks_Of_CloudOps" \
-  --connection-string $Connection_String \
-  --overwrite \
-  --pattern "*.jpeg"
-
+  --pattern "*"
